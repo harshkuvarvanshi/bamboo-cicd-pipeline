e9b151225e95d5424d92473e18d575af8cd8f87c
harsh <harshkuvarvanshi@gmail.cm>
2026-02-26 14:45:22 +0530

    Initial terraform setup

---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+bucket_name = "harsh-terraform-s3-demo-bucket"
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+}
